25b661915aa36cb7c39569ef2b4bf8eec2bcdb29
dusica_trbovic <trbovicdusica@gmail.com>
2023-07-01 15:11:53 +0200

    Background Change

--- Storage/Serializer.cs
+++ Storage/Serializer.cs
@@ -1,10 +1,9 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiTe.Storage
 {
@@ -14,25 +13,75 @@ namespace MiTe.Storage
 
         public void ToJSON(string fileName, List<T> objects)
         {
-            //string json = JsonConvert.SerializeObject(objects, Formatting.Indented,
-            //  new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-            string json_input = JsonConvert.SerializeObject(objects, Newtonsoft.Json.Formatting.Indented);
-            File.WriteAllText(fileName, json_input);
+            var settings = GetJsonSerializerSettings();
+            string json = JsonConvert.SerializeObject(objects, Formatting.Indented, settings);
+            File.WriteAllText(fileName, json);
         }
 
         public List<T> FromJSON(string fileName)
         {
-            StreamReader reader = new StreamReader(fileName);
-            var jsonString = reader.ReadToEnd();
-            var settings = new JsonSerializerSettings
-            {
-                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
-                DateFormatString = "yyyy-MM-ddTHH:mm:ss" // Set the custom date format string for date and time
-            };
+            var settings = GetJsonSerializerSettings();
+            string jsonString = File.ReadAllText(fileName);
             List<T> objects = JsonConvert.DeserializeObject<List<T>>(jsonString, settings);
-
             return objects;
         }
 
+        private JsonSerializerSettings GetJsonSerializerSettings()
+        {
+            var settings = new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                Formatting = Formatting.Indented
+            };
+
+            // Add converters for DateTime, DateOnly, and TimeOnly types
+            var converters = new List<JsonConverter>
+            {
+                new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-ddTHH:mm:ss" }, // DateTime
+                new DateOnlyConverter(), // DateOnly
+                new TimeOnlyConverter() // TimeOnly
+            };
+            settings.Converters = settings.Converters.Concat(converters).ToList();
+
+            return settings;
+        }
+
+    }
+
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return DateOnly.MinValue;
+
+            if (DateTime.TryParse(reader.Value.ToString(), out DateTime dateTime))
+                return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+
+            throw new JsonSerializationException($"Unable to parse {reader.Value} as a valid DateOnly value.");
+        }
+
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString("yyyy-MM-dd"));
+        }
+    }
+    public class TimeOnlyConverter : JsonConverter<TimeOnly>
+    {
+        public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return TimeOnly.MinValue;
+
+            if (TimeSpan.TryParse(reader.Value.ToString(), out TimeSpan timeSpan))
+                return new TimeOnly(timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+
+            throw new JsonSerializationException($"Unable to parse {reader.Value} as a valid TimeOnly value.");
+        }
+
+        public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString());
+        }
     }
 }
